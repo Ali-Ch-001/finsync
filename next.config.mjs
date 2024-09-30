33a618946676f4d36a9cfa7cce0eb6c8821d39c3
Kabeer Ahmad <kabeeahmad.ka@gmail.com>
2024-10-01 02:40:08 +0500

    Added SpinWheel to SideBar

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        ignoreBuildErrors: true
-      },
-      eslint: {
-        ignoreDuringBuilds: true
-      }
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  images: {
+    domains: ['img.icons8.com'], // Add img.icons8.com to the allowed domains
+  },
 };
 
+// Exporting the final configuration
 export default nextConfig;
