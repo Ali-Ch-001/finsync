3a5659f2dcaf7a5c2768e23a47a86f318c9d0797
Ali Mohsin <108975862+Ali-Ch-001@users.noreply.github.com>
2024-11-09 14:04:21 +0500

    fix transactions page and fetch from plaid api issue

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -140,7 +140,7 @@ export const createLinkToken = async (user: User) => {
         client_user_id: user.$id
       },
       client_name: `${user.firstName} ${user.lastName}`,
-      products: ['auth'] as Products[],
+      products: ['auth' , 'transactions'] as Products[],
       language: 'en',
       country_codes: ['US'] as CountryCode[],
     }
